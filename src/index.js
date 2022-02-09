2c730ee4bc2ab3d07393c9cdb88327fb0b6a189a
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-02-08 23:35:16 -0600

    Finished up the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './less/index.less'
 window.onload = function(evt) {
     const heading = document.querySelector('h1');
     heading.textContent = "LEEEEEEET'S GOOOOOO!!!";
+    
 }
 
 const heading = document.querySelector('h1');
@@ -38,3 +39,31 @@ function remover(){
 }
 copyRightRemove.addEventListener('mouseout', remover);
 
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+        .then(text => {
+            console.log(text)
+        })
+})
+
+function mKey(event){
+    if(event.key === 'm'){
+        event.target.classList.toggle('mirror');
+    }
+}
+document.body.addEventListener('keydown', mKey);
+
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY } = evt;
+    console.log(`Mouse is at ${clientX}, ${clientY}.`);
+});
+
+const destinations = document.querySelectorAll('.destination');
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+        destination.style.fontWeight = 'bold';
+    })
+    destination.addEventListener('mouseleave', () => {
+        destination.style.fontWeight = 'initial';
+    })
+}
